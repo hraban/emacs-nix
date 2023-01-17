66c3e5d823b16b67deed508f6c0329aaba39e3ec
Hraban Luyat <hraban@0brg.net>
2023-01-17 11:11:44 +0000

    feat: mac-port from community overlay

--- flake.nix
+++ flake.nix
@@ -20,9 +20,16 @@
       url = "git://git.sv.gnu.org/emacs.git";
       flake = false;
     };
+    community-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
   outputs = {
-    self, nixpkgs, flake-utils, emacs
+    self, nixpkgs, flake-utils, emacs, community-overlay
   }:
     with flake-utils.lib;
     let
@@ -41,13 +48,18 @@
           # itself, re-import that, and finally override the version (and the src
           # attr).
           orig-pkgs = nixpkgs.legacyPackages.${system};
+          # I /think/ there is a better way?
+          ov-pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ community-overlay.overlay ];
+          };
           patched-nixpkgs-src = orig-pkgs.applyPatches {
             name = "nixpkgs-emacs-overridable-version";
             src = nixpkgs;
             patches = [ ./nixpkgs-emacs-overridable-version.patch ];
           };
           patched-pkgs = import patched-nixpkgs-src { inherit system; };
-          from-nix = {
+          all = {
             # The version is passed to the function that constructs this
             # derivation, and it is /that/ version which is used to determine
             # which patches to apply. You can override the version through
@@ -55,24 +67,57 @@
             # the derivation. That won’t influence the choice of patches, which
             # will cause a patch fail (see the emacs derivation in nixpkgs).
             packages = rec {
-              from-nix = (patched-pkgs.emacs.override ({
+              from-nixpkgs = (patched-pkgs.emacs.override ({
                 version = "30.0-git";
               })).overrideAttrs (_: {
                 src = emacs;
               });
-              default = from-nix;
+              default = from-nixpkgs;
             };
             apps = rec {
-              default = from-nix;
+              from-nixpkgs = mkApp { drv = self.packages.${system}.from-nixpkgs; };
+              default = from-nixpkgs;
             };
           };
-          # On mac, those don’t work well. Also offer build from source.
-          from-src = orig-pkgs.lib.attrsets.optionalAttrs (isMac system) {
+          darwin = orig-pkgs.lib.attrsets.optionalAttrs (isMac system) {
             packages = rec {
+              # On mac, those don’t work well. Also offer build from source.
               from-src = import ./from-src-mac.nix {
                 src = emacs;
                 pkgs = orig-pkgs;
               };
+              # And the Nix wiki suggests this approach at
+              # "https://nixos.wiki/wiki/Emacs". Obviously fragile.
+              mac-port = let
+                rev = "6d4b8346773907e42efacbcf5aac0b27b79cc3b9";
+                base = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/${rev}";
+              in
+                ov-pkgs.emacsPgtk.overrideAttrs (old: {
+                  patches =
+                    (old.patches or [])
+                    ++ [
+                      # Fix OS window role (needed for window managers like yabai)
+                      (ov-pkgs.fetchpatch {
+                        url = "${base}/patches/emacs-28/fix-window-role.patch";
+                        sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+                      })
+                      # Use poll instead of select to get file descriptors
+                      (ov-pkgs.fetchpatch {
+                        url = "${base}/patches/emacs-29/poll.patch";
+                        sha256 = "sha256-jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY=";
+                      })
+                      # Enable rounded window with no decoration
+                      (ov-pkgs.fetchpatch {
+                        url = "${base}/patches/emacs-29/round-undecorated-frame.patch";
+                        sha256 = "sha256-qPenMhtRGtL9a0BvGnPF4G1+2AJ1Qylgn/lUM8J2CVI=";
+                      })
+                      # Make Emacs aware of OS-level light/dark mode
+                      (ov-pkgs.fetchpatch {
+                        url = "${base}/patches/emacs-28/system-appearance.patch";
+                        sha256 = "sha256-oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8=";
+                      })
+                    ];
+              });
               default = from-src;
             };
             apps = rec {
@@ -81,7 +126,7 @@
             };
           };
         in {
-          packages = orig-pkgs.lib.trivial.mergeAttrs from-nix.packages (from-src.packages or {});
-          apps = orig-pkgs.lib.trivial.mergeAttrs from-nix.apps (from-src.apps or {});
+          packages = all.packages // (darwin.packages or {});
+          apps = all.apps // (darwin.apps or {});
         });
 }
